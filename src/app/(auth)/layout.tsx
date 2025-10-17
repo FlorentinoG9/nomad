502a2dc0d28c103c2d54962cc09985409c5b55a4
FlorentinoG9 <f.valle.garcia@gmail.com>
2025-10-17 12:50:37 -0500

    layout auth components

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,6 @@
 import type React from "react"
-import { Logo } from "@/features/logo"
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <section className='container mx-auto flex h-svh flex-col items-center justify-center gap-4 overflow-hidden px-4 md:px-0'>
-      <Logo alt='Logo' height={50} src='/logos/nomad.svg' width={50} />
-      {children}
-    </section>
-  )
+import { AuthLayout } from "@/features/auth/components/auth-layout"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <AuthLayout>{children}</AuthLayout>
 }
