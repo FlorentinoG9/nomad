8cc97f25a42f9a45687972454bd8bae2f1b9ac75
FlorentinoG9 <f.valle.garcia@gmail.com>
2025-10-20 13:36:53 -0500

    logo

--- src/features/logo/index.tsx
+++ src/features/logo/index.tsx
@@ -1,10 +1,17 @@
 import Image, { type ImageProps } from "next/image"
 import Link from "next/link"
+import { cn } from "@/lib/utils"
 
-export function Logo({ alt, href = "/", src = "/logos/nomad.svg", ...props }: ImageProps & { href?: string }) {
+export function Logo({
+  alt,
+  href = "/",
+  src = "/logos/nomad.svg",
+  className,
+  ...props
+}: ImageProps & { href?: string }) {
   return (
-    <Link className='flex items-center gap-2' href={href}>
-      <Image alt={alt} className='size-10' height={40} src={src} width={40} {...props} />
+    <Link className={cn("flex items-center gap-2", className)} href={href}>
+      <Image alt={alt} height={40} src={src} width={40} {...props} />
       <span className='font-bold text-4xl'>Nomad</span>
     </Link>
   )
